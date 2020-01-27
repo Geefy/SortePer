e3208f63b8b74ca92fe5c350b8d9b7b77fc0cb10
Geefy <42178995+Geefy@users.noreply.github.com>
2020-01-27 18:59:46 +0100

    Add project files.

---
+++ Sorteper/CpuPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorteper
+{
+    class CpuPlayer : Player
+    {
+        Random rng = new Random();
+        public CpuPlayer(string name) : base(name)
+        {
+        }
+
+        //First checks if players has any cards left
+        //Draws a card from another player, instead of user input this determines the card chosen by using a random
+        public override void DrawFromPlayer(Player player, int index)
+        {
+            if (hand.Count == 0)
+            {
+                Console.WriteLine(playerName + " has no cards left and is out of the game");
+                isOut = true;
+                return;
+            }
+            Console.WriteLine(playerName + " choose a card between 1 and " + player.hand.Count());
+            index = rng.Next(0, player.hand.Count());
+            DrawFromDeck(player.hand.ElementAt(index));
+            player.hand.RemoveAt(index);
+        }
+    }
+}
